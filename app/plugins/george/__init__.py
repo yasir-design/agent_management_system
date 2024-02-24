127a37a7e05e771f2d60a2d8c032eb2805a3c210
kaw393939 <kwilliam@njit.edu>
2024-02-23 19:08:56 -0500

    updated dotenv

--- app/plugins/george/__init__.py
+++ app/plugins/george/__init__.py
@@ -7,15 +7,15 @@ from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
 
 
-class BobCommand(Command):
+class GeorgeCommand(Command):
     def execute(self):
         print("Type 'done' to exit.")
         load_dotenv()
         API_KEY=os.getenv('OPEN_AI_KEY')
         while True:  #REPL Read, Evaluate, Print, Loop
-            llm = ChatOpenAI(openai_api_key="")
+            llm = ChatOpenAI(openai_api_key=API_KEY)
             prompt = ChatPromptTemplate.from_messages([
-            ("system", "Your name is George and you love vanilla ice cream"),
+            ("system", "You Want to know where the user is from and once you know tell them something about their hometown.  If the user asks what you can do, you need to tell them that you can talk about their hometown and ask them where are they from."),
             ("user", "{input}")
         ])
             output_parser = StrOutputParser()
